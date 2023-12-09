7ebe04dde4341c8f9324c5e14ea4860abbcfa279
hariom.kumar <hariom987316@gmail.com>
2023-12-10 02:15:37 +0530

    Adding code

--- src/app/note-input/note-input.component.ts
+++ src/app/note-input/note-input.component.ts
@@ -1,37 +1,69 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-note-input',
   templateUrl: './note-input.component.html',
   styleUrls: ['./note-input.component.css'],
 })
-export class NoteInputComponent implements OnInit {
+export class NoteInputComponent implements OnInit, OnDestroy {
   @Input() inputValue = '';
   textSizeProperty = '0';
   sourceCode: string;
   dev: string;
+  storageType: boolean;
+  textAreaValue = new Subject();
+
   constructor() {
     this.textSizeProperty = '13';
     this.sourceCode = 'View Source Code';
     this.dev = 'developer profile';
+    this.storageType = true;
   }
-  ngOnInit() {}
+
+  ngOnInit() {
+    // logic for retriving the stored notes
+    if (sessionStorage.getItem('notes')) {
+      const _inputValue = sessionStorage.getItem('notes')
+        ? `${sessionStorage.getItem('notes')}`
+        : '';
+      this.inputValue = _inputValue ? JSON.parse(_inputValue) : '';
+      this.storageType = false;
+    } else {
+      const _inputValue = localStorage.getItem('notes')
+        ? `${localStorage.getItem('notes')}`
+        : '';
+      this.inputValue = _inputValue ? JSON.parse(_inputValue) : '';
+    }
+  }
+
   textAreaInput(event: Event) {
     const _inputData = (<HTMLInputElement>event.target)?.value
       ? (<HTMLInputElement>event.target)?.value
       : '';
-    const _scrollLength = (<HTMLInputElement>event.target).scrollHeight;
+    this.textAreaValue.next(_inputData);
+    this.textAreaValue.pipe(debounceTime(1000)).subscribe((data) => {
+      if (this.storageType) {
+        localStorage.setItem('notes', JSON.stringify(data));
+      } else {
+        sessionStorage.setItem('notes', JSON.stringify(data));
+      }
+    });
   }
-  openProjectCode() {
-    window.open('https://github.com/hariomgola/notepad', '_self');
-  }
+
   openPortfolioCode() {
     window.open('https://github.com/hariomgola', '_self');
   }
+
   changeTextSize(event: Event) {
     const _textSizeValue = (<HTMLInputElement>event.target)?.value
       ? (<HTMLInputElement>event.target)?.value
       : 10;
     this.textSizeProperty = `${_textSizeValue}`;
+  }
+
+  ngOnDestroy() {
+    if (this.storageType) sessionStorage.removeItem('notes');
+    this.textAreaValue.complete();
   }
 }
