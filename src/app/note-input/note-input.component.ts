b7e741d267044b87c7007bf10de4e44d9b081bc6
hariom.kumar <hariom987316@gmail.com>
2023-12-09 10:13:16 +0530

    adding row value and css property

--- src/app/note-input/note-input.component.ts
+++ src/app/note-input/note-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note-input',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-input.component.css'],
 })
 export class NoteInputComponent implements OnInit {
+  @Input() inputValue = '';
   textAreaRowsProperty: number;
   previousValue: number;
+  sourceCode = 'View Source Code';
   constructor() {
-    this.textAreaRowsProperty = 2;
+    this.textAreaRowsProperty = 100;
     this.previousValue = 0;
   }
   ngOnInit() {}
@@ -19,10 +21,14 @@ export class NoteInputComponent implements OnInit {
       : '';
     const _scrollLength = (<HTMLInputElement>event.target).scrollHeight;
     // code logic for the expension fo row in textArea
-    const scrollBarRowLengthValue = _scrollLength - this.previousValue;
-    if (scrollBarRowLengthValue == 15 || scrollBarRowLengthValue == 16) {
-      this.textAreaRowsProperty += 1;
-    }
-    this.previousValue = _scrollLength;
+    // const scrollBarRowLengthValue = _scrollLength - this.previousValue;
+    // if (scrollBarRowLengthValue == 15 || scrollBarRowLengthValue == 16) {
+    //   this.textAreaRowsProperty += 1;
+    // }
+    // this.previousValue = _scrollLength;
+    // this.inputValue = _inputData;
+  }
+  openProjectCode() {
+    window.open('https://github.com/hariomgola/notepad', '_self');
   }
 }
